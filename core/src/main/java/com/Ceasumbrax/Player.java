af887431f1a1b1ba2f0961558507aa87c6e6c414
arthu <arthurcormoulshoules@icloud.com>
2026-03-20 19:53:26 +0100

    Adding print statement to see if connected to git

--- core/src/main/java/com/Ceasumbrax/Player.java
+++ core/src/main/java/com/Ceasumbrax/Player.java
@@ -40,8 +40,6 @@ public class Player {
         playerSizeWidth = 38;
         playerSizeHeight = 48;
 
-        System.out.println("Heyy");
-
         playerImage = texture;
         playerImageSide = textureSide;
         playerImageUp = textureUp;
